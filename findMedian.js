076087fce9671f26c56ba52cdea5f27b9d525ed0
Austin Shin <shinaustin@gmail.com>
2018-02-21 10:46:21 -0800

    working on findmedian

---
+++ findMedian.js
@@ -0,0 +1,50 @@
+/*
+ * Find the median in O(1) time
+ * Insertion in O(logn) time
+ * Removal in O(logn) time
+ * Use Heaps
+ */
+
+class PriorityQueue {
+  constructor() {
+  }
+  size() {
+  }
+  isEmpty() {
+  }
+  peek() {
+  }
+  push() {
+  }
+  pop() {
+  }
+  replace() {
+  }
+  _greater(i, j) {
+  }
+  _swap(i, j) {
+  }
+  _siftUp() {
+  }
+  _siftDown() {
+  }
+}
+const queue = new PriorityQueue();
+queue.push(1, 2, 3, 4, 6, 5, 7);
+while (!queue.isEmpty()) {
+  console.log(queue.pop()); //=> 40, 30, 20, 10
+}
+
+class MedianHeap {
+  constructor() {
+  }
+  peekMedian() {
+  }
+  insert() {
+  }
+  size() {
+  }
+}
+
+const medianHeap = new MedianHeap();
+
